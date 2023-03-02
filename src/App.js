b14bada88399c39b178d411b7ea27bf2afb01c4b
Omkar Dhumal <omkardhumal213@gmail.com>
2023-03-02 14:42:19 +0530

    AboutTa, About us, History

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from "react";
+import AboutTA from "./Components/AboutTA";
+import AboutUs from "./Components/AboutUs";
 import Banner from "./Components/Banner";
 import BoardOfMembers from "./Components/Board_of_members";
 import Contact from "./Components/Contact";
 import Footer from "./Components/Footer";
+import HistoryOrigin from "./Components/HistoryOrigin";
 import InfoSection from "./Components/InfoSection";
 import TopHeader from "./Components/TopHeader";
 
@@ -14,6 +17,9 @@ function App() {
       <Banner />
       <InfoSection />
       <BoardOfMembers/>
+      <AboutTA/>
+      <AboutUs/>
+      <HistoryOrigin/>
       <Contact/>
       <Footer/>
     </div>
---
+++ src/Components/HistoryOrigin.jsx
@@ -0,0 +1,44 @@
+import React, { useState, useEffect, useCallback } from "react";
+import { client } from "../client";
+
+const HistoryOrigin = () => {
+  const [info, setInfo] = useState([]);
+
+  const cleanUpInfo = useCallback((rawdata) => {
+    const cleaninfo = rawdata.map((item) => {
+      const { sys, fields } = item;
+      const { id } = sys;
+      const Title = fields.title;
+      const description = fields.description;
+      const updateInfo = {id, Title, description};
+      return updateInfo;
+    });
+    setInfo(cleaninfo);
+  }, []);
+
+  const getInfo = useCallback(async () => {
+    try {
+      const response = await client.getEntries({ content_type: "historyOfSatta" });
+      const responseData = response.items;
+      console.log(responseData)
+      if (responseData) {
+        cleanUpInfo(responseData);
+      } else {
+        setInfo([]);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  },[cleanUpInfo]);
+
+  useEffect(() => {
+    getInfo();
+  }, [getInfo]);
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default HistoryOrigin
