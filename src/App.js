4da5f66c48cb7566b947ffed2f5824fcebd9371e
Himmatlal Parmar <himmatlalparmar785@gmail.com>
2023-03-03 18:03:00 +0530

    add new page

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Home from "./Pages/Home";
 import Membership_Registration from "./Pages/Membreship-registration";
 import About from "./Pages/About";
 import HistoryOrigin from "./Components/HistoryOrigin";
+import Certified_Members from "./Pages/Certified-Members";
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
           <Route exact path='/About' element={<About/>}></Route>
           <Route exact path='/Gallery' element={<Gallery/>}></Route>
           <Route exact path='/Membership_Registration' element={<Membership_Registration/>}></Route>
+          <Route exact path='/Certified_Members' element={<Certified_Members/>}></Route>
           <Route exact path='/AboutUS' element={<AboutUs/>}></Route>
           <Route exact path='/AboutTA' element={<AboutTA/>}></Route>
           <Route exact path='/HistoryOrigin' element={<HistoryOrigin/>}></Route>
