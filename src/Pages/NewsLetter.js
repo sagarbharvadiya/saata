43f99dd69bad5b0d579e911ee47976b14fd0d4a1
Himmatlal Parmar <himmatlalparmar785@gmail.com>
2023-03-23 14:33:23 +0530

    sidebar created

--- src/Pages/NewsLetter.js
+++ src/Pages/NewsLetter.js
@@ -4,10 +4,18 @@ import client from "../client";
 // import ReactHtmlParser from 'react-html-parser';
 import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
 import { BLOCKS, MARKS } from '@contentful/rich-text-types';
+import news_letter_right_image from '../Images/think_tank.png'; 
 
 
 
 const NewsLetter = () => {
+
+  const [modal, sertModal] = useState(false);
+  const newsdropdown = () => { sertModal(!modal) }
+  
+  const [articlemodal, sertModalarticl] = useState(false);
+  const articlesdropdown = () => { sertModalarticl(!articlemodal) }
+
   const { slug } = useParams();
   const [entry, setEntry] = useState([]);
 
@@ -70,11 +78,59 @@ const NewsLetter = () => {
             <React.Fragment key={id}>
               <div className="news-main">
                   <h1>{title}</h1>
-                <div className="news-letter-subtitle">
-                  {richTextContents}
-                </div>
-                <div className="newsletter-description">
-                  {richTextContent}
+                <div className="news-letter-container">
+                    <div className="news-letter-folder1">
+                      <div className="news-letter-subtitle">
+                        {richTextContents}
+                      </div>
+                      <div className="newsletter-description">
+                        {richTextContent}
+                      </div>
+                    </div>
+                    <div className="news-letter-folder2">
+                      <div className="news-latter-right-field-folder">
+                          <div className="news-letter-right-field">
+                            <a href="#">From the Editorial Desk</a>
+                          </div>
+                          <div className="news-letter-dropdown-field">
+                            <div onClick={newsdropdown} className="news-letter-right-field">
+                              <a href="#">TA NEWS</a>
+                            </div>
+                            {modal &&(
+                              <ul onClick={newsdropdown} className="TA-NEWS-dropdown-menu">
+                                <li><a href="#">SAATA Conference Updates</a></li>
+                                <li><a href="#">News from the Research and Publications Team</a></li>
+                                <li><a href="#">Operations Team</a></li>
+                                <li><a href="#">Dev Team Updates</a></li>
+                                <li><a href="#">Miscellaneous News</a></li>
+                            </ul>
+                            )}
+                          </div>
+                          <div className="news-letter-right-field">
+                            <a href="#">Experiences</a>
+                          </div>
+                          <div className="news-letter-dropdown-field">
+                            <div onClick={articlesdropdown} className="news-letter-right-field">
+                              <a href="#">ARTICLES</a>
+                            </div>
+                            {articlemodal &&(
+                              <ul onClick={articlesdropdown} className="TA-NEWS-dropdown-menu">
+                                <li><a href="#">Where There’s a Will There’s a Way</a></li>
+                                <li><a href="#">Unmute Please</a></li>
+                            </ul>
+                            )}
+                          </div>
+                          <div className="news-letter-right-field">
+                            <a href="#">Creative Corner</a>
+                          </div>
+                          <a href="#" className="news-letter-right-field-image">
+                            <img src={news_letter_right_image}/>
+                          </a>
+                          <div className="news-letter-right-field">
+                            <a href="#">Copyright policy</a>
+                          </div>
+                      </div>
+                    </div>
                 </div>
               </div>
               <NavLink to='basicpage/ta-news'>ta news</NavLink>
