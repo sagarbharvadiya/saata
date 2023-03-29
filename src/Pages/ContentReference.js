e884d6b7f3c67f645df6f3c9e79995d8a47c3552
Himmatlal Parmar <himmatlalparmar785@gmail.com>
2023-03-29 14:18:37 +0530

    new logo and chnages css

--- src/Pages/ContentReference.js
+++ src/Pages/ContentReference.js
@@ -38,7 +38,7 @@ const ContentReference = () => {
         <span>VOL: 1, ISSUE: 1 - Dec 2021</span>
         <a href="https://www.facebook.com/saataworld" target='_blank' rel='noreferrer'> <i className="fa-brands fa-facebook"></i></a>
         <div className="newsletter_banner_img">
-          <img src="/Images/banner.png" alt="banner"></img>
+          <img src="/Images/banner-logo.jpg" alt="banner"></img>
         </div>
       </div>
       {
