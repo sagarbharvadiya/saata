a698f6c70ea3ac86fb73dc12f721dc254bbc9cdf
Himmatlal Parmar <himmatlalparmar785@gmail.com>
2023-03-31 14:36:41 +0530

    chnages in downlode form and downlode documnets

--- src/Pages/CertifiedMembers.js
+++ src/Pages/CertifiedMembers.js
@@ -17,28 +17,34 @@ function CertifiedMembers() {
                   <h2>{val.title}</h2>
                   <div className="Certified-Members-details-folder">
                     <div className="Certified-Members-details">
-                      <h2>{val.Qualification1}</h2>
-                      <p>{val.Qualificationdes1}</p>
+                      <li><span>{val.Qualification1}: </span>{val.Qualificationdes1}</li>
+                      {/* <h2>{val.Qualification1}</h2>
+                      <p>{val.Qualificationdes1}</p> */}
                     </div>
                     <div className="Certified-Members-details">
-                      <h2>{val.Qualification2}</h2>
-                      <p>{val.Qualificationdes2}</p>
+                      <li><span>{val.Qualification2} </span>{val.Qualificationdes2}</li>
+                      {/* <h2>{val.Qualification2}</h2>
+                      <p>{val.Qualificationdes2}</p> */}
                     </div>
                     <div className="Certified-Members-details">
-                      <h2>{val.Qualification3}</h2>
-                      <p>{val.Qualificationdes3}</p>
+                      <li><span>{val.Qualification3} </span>{val.Qualificationdes3}</li>
+                      {/* <h2>{val.Qualification3}</h2>
+                      <p>{val.Qualificationdes3}</p> */}
                     </div>
                     <div className="Certified-Members-details">
-                      <h2>{val.Qualification4}</h2>
-                      <p>{val.Qualificationdes4}</p>
+                      <li><span>{val.Qualification4} </span>{val.Qualificationdes4}</li>
+                      {/* <h2>{val.Qualification4}</h2>
+                      <p>{val.Qualificationdes4}</p> */}
                     </div>
                     <div className="Certified-Members-details">
-                      <h2>{val.Qualification5}</h2>
-                      <p>{val.Qualificationdes5}</p>
+                      <li><span>{val.Qualification5} </span>{val.Qualificationdes5}</li>
+                      {/* <h2>{val.Qualification5}</h2>
+                      <p>{val.Qualificationdes5}</p> */}
                     </div>
                     <div className="Certified-Members-details">
-                      <h2>{val.Qualification6}</h2>
-                      <p>{val.Qualificationdes6}</p>
+                      <li><span>{val.Qualification6} </span>{val.Qualificationdes6}</li>
+                      {/* <h2>{val.Qualification6}</h2>
+                      <p>{val.Qualificationdes6}</p> */}
                     </div>
                   </div>
                 </div>
