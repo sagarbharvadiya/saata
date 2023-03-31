21c2389cc8e13761f29b04298be69c33a3ea574a
sagarbharvadiya <sagarbharvadiya@gmail.com>
2023-03-31 19:10:57 +0530

    certified member

--- src/Pages/CertifiedMembers.js
+++ src/Pages/CertifiedMembers.js
@@ -1,55 +1,87 @@
-import React from "react";
-import CertifiedMembersData from "../DataJson/CertifiedMembersData";
+import React, { useState, useEffect } from "react";
+// import { useParams } from "react-router-dom";
+import client from "../client";
+import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
+import { BLOCKS, INLINES } from '@contentful/rich-text-types';
 
 function CertifiedMembers() {
+
+  const [entry, setEntry] = useState([]);
+
+  useEffect(() => {
+    const fetchPage = async () => {
+      try {
+        const response = await client.getEntries({
+          content_type: "certifiedMembers",
+          // "fields.slug": slug,
+        });
+
+        console.log(response)
+        if (response.items.length) {
+          setEntry(response.items);
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchPage();
+  }, []);
   return (
     <>
       <div className="Certified-Members-section">
         <div className="Certified-Members-wrapper">
           <h2 className="Certified-Members-text">Certified Members</h2>
           <div className="Certified-Members-folder">
-            {CertifiedMembersData.map((val) => {
-              return (
-                <div className="Certified-Members-card">
-                  <div className="Certified-Members-image">
-                    <img src={val.image} alt={val.image} />
-                  </div>
-                  <h2>{val.title}</h2>
-                  <div className="Certified-Members-details-folder">
-                    <div className="Certified-Members-details">
-                      <li><span>{val.Qualification1}: </span>{val.Qualificationdes1}</li>
-                      {/* <h2>{val.Qualification1}</h2>
-                      <p>{val.Qualificationdes1}</p> */}
-                    </div>
-                    <div className="Certified-Members-details">
-                      <li><span>{val.Qualification2} </span>{val.Qualificationdes2}</li>
-                      {/* <h2>{val.Qualification2}</h2>
-                      <p>{val.Qualificationdes2}</p> */}
-                    </div>
-                    <div className="Certified-Members-details">
-                      <li><span>{val.Qualification3} </span>{val.Qualificationdes3}</li>
-                      {/* <h2>{val.Qualification3}</h2>
-                      <p>{val.Qualificationdes3}</p> */}
-                    </div>
-                    <div className="Certified-Members-details">
-                      <li><span>{val.Qualification4} </span>{val.Qualificationdes4}</li>
-                      {/* <h2>{val.Qualification4}</h2>
-                      <p>{val.Qualificationdes4}</p> */}
-                    </div>
-                    <div className="Certified-Members-details">
-                      <li><span>{val.Qualification5} </span>{val.Qualificationdes5}</li>
-                      {/* <h2>{val.Qualification5}</h2>
-                      <p>{val.Qualificationdes5}</p> */}
-                    </div>
-                    <div className="Certified-Members-details">
-                      <li><span>{val.Qualification6} </span>{val.Qualificationdes6}</li>
-                      {/* <h2>{val.Qualification6}</h2>
-                      <p>{val.Qualificationdes6}</p> */}
-                    </div>
-                  </div>
-                </div>
-              );
-            })}
+            {
+              entry.map((item) => {
+                const { qualification, name, } = item.fields;
+                const id = item.sys.id
+                console.log(id)
+                const richTextContent = documentToReactComponents(qualification, {
+                  renderNode: {
+                    [INLINES.ASSET_HYPERLINK]: (node) => (
+                      <a href={`https://` + node.data.target.fields.file.url} target="_blank" rel="noopener noreferrer">{node.data.target.fields.title}</a>
+
+                    ),
+                    [BLOCKS.EMBEDDED_ASSET]: (node) => (
+                      <img
+                        src={`https:${node.data.target.fields.file.url}`}
+                        alt={node.data.target.fields.description}
+                      />
+
+                    ),
+                  },
+                });
+                const richTextName = documentToReactComponents(name, {
+                  renderNode: {
+                    [INLINES.ASSET_HYPERLINK]: (node) => (
+                      <a href={`https://` + node.data.target.fields.file.url} target="_blank" rel="noopener noreferrer">{node.data.target.fields.title}</a>
+
+                    ),
+                    [BLOCKS.EMBEDDED_ASSET]: (node) => (
+                      <img
+                        src={`https:${node.data.target.fields.file.url}`}
+                        alt={node.data.target.fields.description}
+                      />
+
+                    ),
+                  },
+                });
+                return (
+                  <React.Fragment key={id}> 
+                  <div>
+                      <div>
+                        {richTextContent}
+                      </div>
+                      <div>
+                        {richTextName}
+                      </div>
+                    </div>    
+                   
+                  </React.Fragment>
+                )
+              })
+            }
           </div>
         </div>
       </div>
