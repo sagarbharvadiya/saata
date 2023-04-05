8d6d9834612bc1f2c1c15d17dd43096dfb949b2e
Himmatlal Parmar <himmatlalparmar785@gmail.com>
2023-04-05 18:13:10 +0530

    chnages in membershMembershipRegistration

--- src/Pages/MembershipRegistration.js
+++ src/Pages/MembershipRegistration.js
@@ -3,12 +3,14 @@ import React from "react";
 const MembershipRegistration = () => {
   return (
     <>
+      <div className="membership-ragistaion">
       <h4 className="text-align-center" data-placeholder="Translation" dir="ltr" id="tw-target-text">
         If you are already an existing SAATA Member, <a href="https://subscriptions.zoho.in/portal/saata1/login">Click here to login</a>
       </h4>
       <div id="zf-widget-root-id"></div>
       <script type="text/javascript" src='https://js.zohostatic.com/books/zfwidgets/assets/js/zf-widget.js'></script>
       <script src="path/to/pricing-table.js"></script>
+      </div>
     </>
   );
 };
