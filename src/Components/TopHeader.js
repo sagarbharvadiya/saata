7a0685e2311b2dc33f78e41a305424b898ea37c2
sagarbharvadiya <sagarbharvadiya@gmail.com>
2023-06-01 19:20:24 +0530

    some changes

--- src/Components/TopHeader.js
+++ src/Components/TopHeader.js
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 import logo from '../Images/logo.png';
 import { Link, NavLink } from "react-router-dom";
 
-
 const TopHeader = () => {
   const [menuOpen, setMenuOpen] = useState(false);
 
@@ -11,88 +10,196 @@ const TopHeader = () => {
   };
 
   return (
-    <>
-      <div className="Top-header-section-folder">
-        <div className="Top-Header-wrapper-folder">
-          <div className="logo">
-            <Link to='/'><img src={logo} alt="Logo" /></Link>
-          </div>
-          <button className="menu-toggle" onClick={toggleMenu}>
-            <div className="menu-toggle-bar"></div>
-            <div className="menu-toggle-bar"></div>
-            <div className="menu-toggle-bar"></div>
-          </button>
-          <ul className={`menu-folder ${menuOpen ? 'menu-open' : ''}`}>
-            <li className="Certifications menu_item">
-              <NavLink to="">Certifications</NavLink>
-              <ul className="Certifications-folder">
-                <li><NavLink to="/basicpage/exam-dates" onClick={() => toggleMenu(false)}>Exam Dates</NavLink></li>
-                <li><NavLink to="/basicpage/download-documents" onClick={() => toggleMenu(false)}>Download Documents</NavLink></li>
-                <li><NavLink to="/basicpage/download-forms" onClick={() => toggleMenu(false)}>Download Forms</NavLink></li>
-              </ul>
-            </li>
-            <li className="Membership menu_item">
-              <NavLink to="">Membership</NavLink>
-              <ul className="Membership-folder">
-                <li><NavLink to='/basicpage/membership-details' onClick={() => toggleMenu(false)}>Membership Details</NavLink></li>
-                <li><NavLink to="/membershipRegistration" onClick={() => toggleMenu(false)}>Registration</NavLink></li>
-                <li><NavLink to="/basicpage/membership-renewal" onClick={() => toggleMenu(false)}>Renewal / Login</NavLink></li>
-              </ul>
-            </li>
-            <li className="directory menu_item">
-              <NavLink to="/basicpage/directory" >Directory</NavLink>
-              <ul className="directory-folder" onClick={() => toggleMenu(false)}>
-                <li><NavLink to="/CertifiedMembers" onClick={() => toggleMenu(false)}>Certified Members</NavLink></li>
-                {/* <li><NavLink to="">Members</NavLink></li> */}
-              </ul>
-            </li>
-            <li className="Programs menu_item">
-              <NavLink to="">Programs</NavLink>
-              <ul className="Programs-folder">
-                <li><NavLink to='/basicpage/ta-101-program-details' onClick={() => toggleMenu(false)}>TA 101 - Program Details</NavLink></li>
-                <li><NavLink to="/basicpage/saata-calendar-2021-2022" onClick={() => toggleMenu(false)}>SAATA Calendar 2021 - 2022</NavLink></li>
-              </ul>
-            </li>
-            <li className="Awards menu_item">
-              <NavLink to="">Awards</NavLink>
-              <ul className="Awards-folder">
-                <li><NavLink to="/basicpage/awards" onClick={() => toggleMenu(false)}>Professional Excellence Award</NavLink></li>
-                <li><NavLink to="/basicpage/service-award" onClick={() => toggleMenu(false)}>Service Awards</NavLink></li>
-                <li><NavLink to="/basicpage/call-for-nominations" onClick={() => toggleMenu(false)}>Call for Nominations</NavLink></li>
-              </ul>
-            </li>
-            <li className="menu_item">
-              <NavLink to="/Gallery" onClick={() => toggleMenu(false)}>Gallery</NavLink>
-            </li>
-            <li className="Newsletter menu_item">
-              <NavLink to='/newsletterlist/' >Newsletter</NavLink>
-              <ul className="Newsletter-folder">
-                <li><NavLink to='/newsletterlist/' onClick={() => toggleMenu(false)}>Current and Previous Issues</NavLink></li>
-              </ul>
-            </li>
-            <li className="Newsletter menu_item">
-              <NavLink>Publications</NavLink>
-              <ul className="Newsletter-folder">
-                <li>
-                  <NavLink to="/basicpage/journals" onClick={() => toggleMenu(false)}>Journals</NavLink>
-                </li>
-              </ul>
-            </li>
-            <li className="Newsletter menu_item">
-              <NavLink>Resources </NavLink>
-              <ul className="Newsletter-folder">
-                <li>
-                  <NavLink to="/Articles" onClick={() => toggleMenu(false)}>Articles</NavLink>
-                </li>
-              </ul>
-            </li>
-            <li className="login_btn">
-              <a href="https://subscriptions.zoho.in/portal/saata1/login" target="_blank" rel="noreferrer" onClick={() => toggleMenu(false)}>Membership Details</a>
-            </li>
-          </ul>
+    <div className="Top-header-section-folder">
+      <div className="Top-Header-wrapper-folder">
+        <div className="logo">
+          <Link to='/'><img src={logo} alt="Logo" /></Link>
         </div>
+        <div className="social_icon">
+            <a
+              href="https://www.facebook.com/saataworld"
+              target="_blank"
+              rel="noreferrer"
+            >
+              {" "}
+              <i className="fa-brands fa-facebook"></i>
+            </a>
+            <a href="https://www.instagram.com/saata_org/?igshid=MzRlODBiNWFlZA%3D%3D"
+            target="_blank"
+            rel="noreferrer">
+              {""}
+              <i class="fa-brands fa-instagram"></i>
+            </a>
+            <a
+              href="https://twitter.com/saataorg"
+              target="_blank"
+              rel="noreferrer"
+            >
+              {" "}
+              <i className="fa-brands fa-twitter"></i>
+            </a>
+            <a
+              href="https://www.youtube.com/channel/UCGTMW3BmOElfJNNoUH2x9JA/videos"
+              target="_blank"
+              rel="noreferrer"
+            >
+              {" "}
+              <i className="fa-brands fa-youtube"></i>
+            </a>
+          </div>
+        <button className="menu-toggle" onClick={toggleMenu}>
+          <div className="menu-toggle-bar"></div>
+          <div className="menu-toggle-bar"></div>
+          <div className="menu-toggle-bar"></div>
+        </button>
+        <ul className={`menu-folder ${menuOpen ? 'menu-open' : ''}`}>
+          <li className="Certifications menu_item">
+            <NavLink to="/">Certifications</NavLink>
+            <ul className="Certifications-folder">
+              <li>
+                <NavLink to="/basicpage/examination-documents" onClick={toggleMenu}>
+                  Examination Documents
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/basicpage/examination-forms" onClick={toggleMenu}>
+                  Examination forms
+                </NavLink>
+              </li>
+            </ul>
+          </li>
+          <li className="Membership menu_item">
+            <NavLink to="/">Membership</NavLink>
+            <ul className="Membership-folder">
+              <li>
+                <NavLink to="/basicpage/membership-details" onClick={toggleMenu}>
+                  Membership Details
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/membershipRegistration" onClick={toggleMenu}>
+                  Registration
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/basicpage/membership-renewal" onClick={toggleMenu}>
+                  Renewal / Login
+                </NavLink>
+              </li>
+            </ul>
+          </li>
+          <li className="directory menu_item">
+            <NavLink to="/basicpage/directory">Directory</NavLink>
+            <ul className="directory-folder" onClick={toggleMenu}>
+              <li>
+                <NavLink to="/CertifiedMembers">Certified Members</NavLink>
+              </li>
+            </ul>
+          </li>
+          <li className="Programs menu_item">
+            <NavLink to="/">Events</NavLink>
+            <ul className="Programs-folder">
+              <li>
+                <NavLink to="/basicpage/saata-calendar-2021-2022" onClick={toggleMenu}>
+                  SAATA Calendar 2023
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/basicpage/ta-101-program-details" onClick={toggleMenu}>
+                  TA 101 - Program Details
+                </NavLink>
+              </li>
+            </ul>
+          </li>
+          <li className="Awards menu_item">
+            <NavLink to="/">Awards</NavLink>
+            <ul className="Awards-folder">
+              <li>
+                <NavLink to="/basicpage/awards" onClick={toggleMenu}>
+                  Professional Excellence Award
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/basicpage/service-award" onClick={toggleMenu}>
+                  Service Awards
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/basicpage/call-for-nominations" onClick={toggleMenu}>
+                  Call for Nominations
+                </NavLink>
+              </li>
+            </ul>
+          </li>
+          <li className="menu_item">
+            <NavLink to="/Gallery" onClick={toggleMenu}>
+              Gallery
+            </NavLink>
+          </li>
+          <li className="Newsletter menu_item">
+            <NavLink to="/">Publications</NavLink>
+            <ul className="Newsletter-folder">
+              <li className="News_hover">
+                <NavLink to="/">Newsletter</NavLink>
+                <ul className="hover">
+                  <li>
+                    <NavLink to="/newsletterTeam/truth-seeker" onClick={toggleMenu}>
+                      Newsletter Team
+                    </NavLink>
+                  </li>
+                  <li>
+                    <NavLink to="/newsletterlist/" onClick={toggleMenu}>
+                      Current and Previous Issues
+                    </NavLink>
+                  </li>
+                  <li>
+                    <NavLink to="/basicpage/guidelines-for-content-contributors" onClick={toggleMenu}>
+                      Guidelines
+                    </NavLink>
+                  </li>
+                  <li>
+                    <NavLink to="/basicpage/newsletter-advertisement-policy" onClick={toggleMenu}>
+                      Advertisement Policy
+                    </NavLink>
+                  </li>
+                  {/* <li>
+                    <NavLink to="/basicpage/team-that-was" onClick={toggleMenu}>
+                      Team that Was
+                    </NavLink>
+                  </li> */}
+                </ul>
+              </li>
+              <li>
+                <NavLink to="/basicpage/journals" onClick={toggleMenu}>
+                  SAJTA Journals
+                </NavLink>
+              </li>
+            </ul>
+          </li>
+          <li className="Newsletter menu_item">
+            <NavLink to="/">Resources</NavLink>
+            <ul className="Newsletter-folder">
+              <li>
+                <NavLink to="/Articles" onClick={toggleMenu}>
+                  Articles
+                </NavLink>
+              </li>
+            </ul>
+          </li>
+          <li className="login_btn">
+            <a
+              href="https://subscriptions.zoho.in/portal/saata1/login"
+              target="_blank"
+              rel="noreferrer"
+              onClick={toggleMenu}
+              title="Click here to login to Zoho Portal to view member details"
+            >
+              Member Login
+            </a>
+          </li>
+        </ul>
       </div>
-    </>
+    </div>
   );
 };
 
