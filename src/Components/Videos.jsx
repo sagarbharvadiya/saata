188c42b6bf1965d2809783ef438a740a1744ed18
krushna53 <102353316+krushna53@users.noreply.github.com>
2023-06-14 18:39:56 +0530

    Update Videos.jsx updated youtube url

--- src/Components/Videos.jsx
+++ src/Components/Videos.jsx
@@ -9,7 +9,7 @@ const Videos = () => {
     <iframe
         width="560"
         height="315"
-        src="https://youtu.be/3dGHvioT_s4"
+        src="https://www.youtube.com/embed/3dGHvioT_s4"
         title="YouTube video player"
         frameborder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
